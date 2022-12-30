361ba7210da35177086c107489ee67576145c15c
scalanakamoto <113780639+scalanakamoto@users.noreply.github.com>
2022-12-30 16:03:47 +0100

    fixed alignment of fees components

--- blocktimeAndFees.js
+++ blocktimeAndFees.js
@@ -1,13 +1,13 @@
 /* --------------------------------------------------------------
+Idea: DerNodetar
 Author: scalanakamoto
-Version: 0.1.0
+Version: 1.0
 -------------------------------------------------------------- */
 
 const urlSkeleton         = 'https://mempool.space/api'
 const urlCurrentBlockTime = `${urlSkeleton}/blocks/tip/height`
 const urlFees             = `${urlSkeleton}/v1/fees/recommended`
-const urlIcon             =
-  'https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/128/Bitcoin-BTC-icon.png'
+const urlIcon             = 'https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/128/Bitcoin-BTC-icon.png'
 
 const reqCurrentBlockTime = new Request(urlCurrentBlockTime);
 const currentBlockTime    = await reqCurrentBlockTime.loadString();
@@ -19,57 +19,62 @@ const bitcoinIcon         = await reqBitcoinIcon.loadImage();
 const createWidget = (blockTime, fees, bitcoinIcon) => {
   const w           = new ListWidget()
   w.backgroundColor = new Color("#000000")
-
+  
   const image     = w.addImage(bitcoinIcon)
   image.imageSize = new Size(45, 45)
   image.centerAlignImage()
-
+  
   w.addSpacer(8)
-
+  
   const blockTimeText     = w.addText("block time")
   blockTimeText.textColor = Color.white()
   blockTimeText.font      = Font.boldSystemFont(12)
   blockTimeText.centerAlignText()
-
+  
   w.addSpacer(8)
-
+  
   const blockTimeValue     = w.addText(blockTime)
   blockTimeValue.textColor = Color.orange()
   blockTimeValue.font      = Font.systemFont(16)
   blockTimeValue.centerAlignText()
-
+  
   w.addSpacer(8)
-
-  const fastestFeeText     = w.addText('median fee in Sats')
-  fastestFeeText.textColor = Color.white()
-  fastestFeeText.font      = Font.boldSystemFont(12)
-  fastestFeeText.centerAlignText()
-
-  const economyFeeText     = w.addText('median fee in Sats')
-  economyFeeText.textColor = Color.white()
-  economyFeeText.font      = Font.boldSystemFont(12)
-  economyFeeText.centerAlignText()
-
-  const minimumFeeText     = w.addText('median fee in Sats')
-  minimumFeeText.textColor = Color.white()
-  minimumFeeText.font      = Font.boldSystemFont(12)
-  minimumFeeText.centerAlignText()
-
-  w.addSpacer(8)
-
-  const fastestFees             = w.addText(fees.fastestFee.toString())
-  const economyFee              = w.addText(fees.economyFee.toString())
-  const minimumFee              = w.addText(fees.minimumFee.toString())
-  fastestFees.textColor         = Color.orange()
-  fastestFees.font              = Font.systemFont(16)
-  fastestFees.centerAlignText()
-  economyFee.textColor          = Color.orange()
-  economyFee.font               = Font.systemFont(16)
-  economyFee.centerAlignText()
-  minimumFee.textColor          = Color.orange()
-  minimumFee.font               = Font.systemFont(16)
-  minimumFee.centerAlignText()
-
+  
+  const feeTextFow = w.addStack();
+  feeTextFow.layoutHorizontally();
+  feeTextFow.addSpacer(1);
+  
+  {
+    const col = feeTextFow.addStack();
+    col.layoutVertically();
+    const text = col.addText('     fast   |')
+    text.font = Font.mediumSystemFont(12);
+    text.textColor = Color.white();
+    let fee = col.addText(`      ${fees.fastestFee.toString()}     `)
+    fee.textColor = Color.orange();
+    fee.font = Font.mediumSystemFont(12);
+  }
+  {
+    const col = feeTextFow.addStack();
+    col.layoutVertically();
+    const text = col.addText('     eco    |');
+    text.font = Font.mediumSystemFont(12);
+    text.textColor = Color.white();
+    let fee = col.addText(`       ${fees.economyFee.toString()}     `);
+    fee.textColor = Color.orange();
+    fee.font = Font.mediumSystemFont(12);
+  }
+  {
+    const col = feeTextFow.addStack();
+    col.layoutVertically();
+    const text = col.addText('   min     ');
+    text.font = Font.mediumSystemFont(12);
+    text.textColor = Color.white();
+    let fee = col.addText(`     ${fees.minimumFee.toString()}      `);
+    fee.textColor = Color.orange();
+    fee.font = Font.mediumSystemFont(12);
+  }
+  
   w.setPadding(0, 0, 0, 0)
   return w
 }
