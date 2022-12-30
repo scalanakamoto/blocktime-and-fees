ba0749747ea74591575558f241dad2d80cf60cb9
scalanakamoto <113780639+scalanakamoto@users.noreply.github.com>
2022-12-30 15:28:28 +0100

    adjustment with fastest, economy, minimum fees

--- blocktimeAndFees.js
+++ blocktimeAndFees.js
@@ -3,20 +3,20 @@ Author: scalanakamoto
 Version: 0.1.0
 -------------------------------------------------------------- */
 
-const urlSkeleton             = 'https://mempool.space/api'
-const urlPathCurrentBlockTime = `${urlSkeleton}/blocks/tip/height`
-const urlMempoolBlocks        = `${urlSkeleton}/v1/fees/mempool-blocks`
-const urlIcon                 =
+const urlSkeleton         = 'https://mempool.space/api'
+const urlCurrentBlockTime = `${urlSkeleton}/blocks/tip/height`
+const urlFees             = `${urlSkeleton}/v1/fees/recommended`
+const urlIcon             =
   'https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/128/Bitcoin-BTC-icon.png'
 
-const reqCurrentBlockTime = new Request(urlPathCurrentBlockTime);
+const reqCurrentBlockTime = new Request(urlCurrentBlockTime);
 const currentBlockTime    = await reqCurrentBlockTime.loadString();
-const reqMempoolBlocks    = new Request(urlMempoolBlocks);
-const mempoolBlock        = await reqMempoolBlocks.loadJSON();
+const reqFees             = new Request(urlFees);
+const currentFees         = await reqFees.loadJSON();
 const reqBitcoinIcon      = new Request(urlIcon);
 const bitcoinIcon         = await reqBitcoinIcon.loadImage();
 
-const createWidget = (blockTime, mempoolBlock, bitcoinIcon) => {
+const createWidget = (blockTime, fees, bitcoinIcon) => {
   const w           = new ListWidget()
   w.backgroundColor = new Color("#000000")
 
@@ -40,22 +40,40 @@ const createWidget = (blockTime, mempoolBlock, bitcoinIcon) => {
 
   w.addSpacer(8)
 
-  const medianFeeText     = w.addText('median fee in Sats')
-  medianFeeText.textColor = Color.white()
-  medianFeeText.font      = Font.boldSystemFont(12)
-  medianFeeText.centerAlignText()
+  const fastestFeeText     = w.addText('median fee in Sats')
+  fastestFeeText.textColor = Color.white()
+  fastestFeeText.font      = Font.boldSystemFont(12)
+  fastestFeeText.centerAlignText()
+
+  const economyFeeText     = w.addText('median fee in Sats')
+  economyFeeText.textColor = Color.white()
+  economyFeeText.font      = Font.boldSystemFont(12)
+  economyFeeText.centerAlignText()
+
+  const minimumFeeText     = w.addText('median fee in Sats')
+  minimumFeeText.textColor = Color.white()
+  minimumFeeText.font      = Font.boldSystemFont(12)
+  minimumFeeText.centerAlignText()
 
   w.addSpacer(8)
 
-  const medianFee     = w.addText(Math.round(mempoolBlock[0].medianFee).toString())
-  medianFee.textColor = Color.orange()
-  medianFee.font      = Font.systemFont(16)
-  medianFee.centerAlignText()
+  const fastestFees             = w.addText(fees.fastestFee.toString())
+  const economyFee              = w.addText(fees.economyFee.toString())
+  const minimumFee              = w.addText(fees.minimumFee.toString())
+  fastestFees.textColor         = Color.orange()
+  fastestFees.font              = Font.systemFont(16)
+  fastestFees.centerAlignText()
+  economyFee.textColor          = Color.orange()
+  economyFee.font               = Font.systemFont(16)
+  economyFee.centerAlignText()
+  minimumFee.textColor          = Color.orange()
+  minimumFee.font               = Font.systemFont(16)
+  minimumFee.centerAlignText()
 
   w.setPadding(0, 0, 0, 0)
   return w
 }
 
 // main
-const widget = createWidget(currentBlockTime, mempoolBlock, bitcoinIcon)
+const widget = createWidget(currentBlockTime, currentFees, bitcoinIcon)
 if (config.runsInWidget) { Script.setWidget(widget); Script.complete() } else { widget.presentSmall() }
